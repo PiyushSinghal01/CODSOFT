cef28254eb0ed62e0cbd9f6dc08db091a61d823f
Piyush Singhal <singhalpiyush789@gmail.com>
2024-07-10 01:08:28 +0530

    first commit

---
+++ Task1/landing-page/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+import './Navbar.css'
+import underline from '../../assets/nav_underline.svg'
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
+import theme_pattern from "../../assets/theme_pattern.svg"
+
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home")
+  const [navToggle, setNavToggle] = useState("");
+
+
+  return (
+    <div id='home' className='navbar'>
+      <div className="nav-title">
+        <h1>Piyush</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      {/* <img src={logo} alt="logo" /> */}
+      <img src={menu_open} onClick={() => setNavToggle("active")} alt="" className='nav-mob-open' />
+      <ul className={`nav-menu ${navToggle}`}>
+      <img src={menu_close} onClick={() => setNavToggle("")} alt="" className="nav-mob-close" />
+        <li><p onClick={() => setMenu("home")}>Home</p>{menu=="home"?<img src={underline} />:""}</li>
+        <li><p onClick={() => setMenu("About")}>About Me</p>{menu=="About"?<img src={underline} />:""}</li>
+        <li><p onClick={() => setMenu("Services")}>Services</p>{menu=="Services"?<img src={underline} />:""}</li>
+        <li><p onClick={() => setMenu("Portfolio")}>Work</p>{menu=="Portfolio"?<img src={underline} />:""}</li>
+        <li><p onClick={() => setMenu("Contact")}>Contact</p>{menu=="Contact"?<img src={underline} />:""}</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
+    </div>
+  )
+}
+
+export default Navbar
