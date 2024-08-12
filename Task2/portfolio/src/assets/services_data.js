b01fb9547207ef140528dd687d2254d9df2a8aad
Piyush Singhal <singhalpiyush789@gmail.com>
2024-08-13 01:58:38 +0530

    service data change

--- Task2/portfolio/src/assets/services_data.js
+++ Task2/portfolio/src/assets/services_data.js
@@ -2,17 +2,17 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Frontend Development",
-        s_desc:"A Front-End Developer is someone who creates websites and web applications. You can think of Front-End as client-side and Back-End as server-side."
+        s_desc:"A Front-End Developer is someone who creates websites and web applications. You can think of Front-End as client-side and Back-End as server-side...."
     },
     {
         s_no:"02",
         s_name:"Backend Development",
-        s_desc:"Back-end development means working on server-side software, which focuses on everything you can't see on a website. Back-end developers ensure the website performs correctly, focusing on databases, back-end logic, application programming interface (APIs), architecture, and servers."
+        s_desc:"Back-end development means working on server-side software, which focuses on everything you can't see on a website...."
     },
     {
         s_no:"03",
         s_name:"Java Development",
-        s_desc:"Java is a widely-used programming language for coding web applications. It has been a popular choice among developers for over two decades, with millions of Java applications in use today. Java is a multi-platform, object-oriented, and network-centric language that can be used as a platform in itself. It is a fast, secure, reliable programming language for coding everything from mobile apps and enterprise software to big data applications and server-side technologies."
+        s_desc:"Java is a widely-used programming language for coding web applications...."
     },
     // {
     //     s_no:"04",
